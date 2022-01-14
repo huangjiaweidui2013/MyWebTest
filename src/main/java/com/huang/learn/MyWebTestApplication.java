76a9e5ed54ae607b7371eb65565aa3010f3abb57
huangjiaweidui2013 <1037084308@qq.com>
2022-01-14 09:11:46 +0800

    初版

---
+++ src/main/java/com/huang/learn/MyWebTestApplication.java
@@ -0,0 +1,18 @@
+package com.huang.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: huang lang
+ * @description: web测试
+ * @date: 2022/1/12 17:53
+ */
+@SpringBootApplication
+public class MyWebTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/huang/learn/controller/FileController.java
@@ -0,0 +1,10 @@
+package com.huang.learn.controller;
+
+/**
+ * @author: huang lang
+ * @description: 文件接口
+ * @date: 2022/1/14 8:49
+ */
+public class FileController {
+
+}
